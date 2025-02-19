83d5c9d2228365f3b1b8eada8e02a39a00ea53fa
Magnus <magnuseifr@gmail.com>
2025-02-19 09:46:15 +0100

    chore: merge da from main

--- da/da.h
+++ da/da.h
@@ -2,24 +2,27 @@
 #define CSTUFF_DA_H
 
 #include <stddef.h>
+#include <stdlib.h>
 
 #define DA_PUSH_BACK_INT(da, val) { int __tmp = val; da_push_back(da, &__tmp); }
 #define DA_AT(da, index, type) (*(type *)da_at(da, index))
 
-struct da_t {
+typedef struct {
     size_t size;
     size_t capacity;
     size_t elem_size;
     void* buffer;
-};
+} da_t;
 
-void  da_init(struct da_t* da, size_t elem_size);
-void  da_deinit(struct da_t* da);
-void  da_resize(struct da_t* da, size_t size);
-void  da_reserve(struct da_t* da, size_t capacity);
-void* da_at(struct da_t* da, size_t index);
-void  da_push_back(struct da_t* da, void* data);
-void  da_pop_back(struct da_t* da);
-size_t da_indexof(struct da_t* da, void* element);
+void  da_init(da_t* da, size_t elem_size);
+void  da_deinit(da_t* da);
+void  da_resize(da_t* da, size_t size);
+void  da_reserve(da_t* da, size_t capacity);
+void* da_at(da_t* da, size_t index);
+void  da_push_back(da_t* da, void* data);
+void  da_pop_back(da_t* da);
+void  da_clear(da_t* da);
+void  da_sort(da_t* da, __compar_fn_t compar);
+size_t da_indexof(da_t* da, void* element);
 
 #endif
